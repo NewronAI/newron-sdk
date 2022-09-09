fbae7809a0b3e7de3a64797665373dec65b37787
djokester <Samriddhi Sinha>
2022-09-09 17:45:50 +0530

    Added Changes for Spacy and Prophet

--- newron/spacy.py
+++ newron/spacy.py
@@ -153,12 +153,12 @@ def load_model(model_uri, dst_path=None):
     Load a spaCy model from a local file (if ``run_id`` is ``None``) or a run.
     Args:    
         model_uri: The location, in URI format, of the MLflow model. For example:
-                      - ``Users\me\path\to\local\model``
-                      - ``relative\path\to\local\model``
-                      - ``s3:\\my_bucket\path\to\model``
-                      - ``runs:\<mlflow_run_id>\run-relative\path\to\model``
-                      - ``models:\<model_name>\<model_version>``
-                      - ``models:\<model_name>\<stage>``
+                      * ``Users\me\path\to\local\model``
+                      * ``relative\path\to\local\model``
+                      * ``s3:\\my_bucket\path\to\model``
+                      * ``runs:\<mlflow_run_id>\run-relative\path\to\model``
+                      * ``models:\<model_name>\<model_version>``
+                      * ``models:\<model_name>\<stage>``
                       For more information about supported URI schemes, see
                       `Referencing Artifacts <https:\\www.mlflow.org\docs\latest\concepts.html#
                       artifact-locations>`.
