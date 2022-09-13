25707f4d6ef86ed487e3c256112d2ff9384a8337
pbnewron <109150046+pbnewron@users.noreply.github.com>
2022-09-13 15:37:14 +0200

    removing autolog from prophet

--- newron/prophet.py
+++ newron/prophet.py
@@ -4,7 +4,7 @@ _load_model = mlflow.prophet._load_model
 _ProphetModelWrapper = mlflow.prophet._ProphetModelWrapper
 _load_pyfunc = mlflow.prophet._load_pyfunc
 _save_model = mlflow.prophet._save_model
-autolog = mlflow.prophet.autolog
+
 
 save_model = mlflow.prophet.save_model
 log_model = mlflow.prophet.log_model
