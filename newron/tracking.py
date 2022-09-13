b2aca22caa0b9e7cf45d9aa3ab0a7fd338787fd6
pbnewron <109150046+pbnewron@users.noreply.github.com>
2022-09-13 10:42:54 +0200

    project url corrected

--- newron/tracking.py
+++ newron/tracking.py
@@ -47,7 +47,7 @@ get_experiment_by_name = mlflow.tracking.fluent.get_experiment_by_name
 list_experiments = mlflow.tracking.fluent.list_experiments
 
 SERVER_URI = "https://mlflow-tracking-server-xylemqanca-uc.a.run.app"
-PROJECT_URI = "https://api.newron.ai/v1/set/project"
+PROJECT_URI = "https://api.newron.ai/v1/project"
 DEFAULT_AWAIT_MAX_SLEEP_SECONDS = 5 * 60
 
 class NewronPluginRequestHeaderProvider(RequestHeaderProvider):
